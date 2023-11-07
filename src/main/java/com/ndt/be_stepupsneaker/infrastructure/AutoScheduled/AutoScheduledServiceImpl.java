1308df42a7583f6cf8db569201a8424cf13d0524
haunguyen284 <nguyenthph29751@fpt.edu.vn>
2023-11-07 11:23:14 +0700

    fix schedule order

--- src/main/java/com/ndt/be_stepupsneaker/infrastructure/AutoScheduled/AutoScheduledServiceImpl.java
+++ src/main/java/com/ndt/be_stepupsneaker/infrastructure/AutoScheduled/AutoScheduledServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ndt.be_stepupsneaker.infrastructure.AutoScheduled;
 
+import com.ndt.be_stepupsneaker.core.admin.repository.order.AdminOrderHistoryRepository;
 import com.ndt.be_stepupsneaker.core.admin.repository.order.AdminOrderRepository;
 import com.ndt.be_stepupsneaker.core.admin.repository.voucher.AdminVoucherRepository;
 import com.ndt.be_stepupsneaker.entity.order.Order;
@@ -7,23 +8,28 @@ import com.ndt.be_stepupsneaker.entity.voucher.Voucher;
 import com.ndt.be_stepupsneaker.infrastructure.constant.OrderStatus;
 import com.ndt.be_stepupsneaker.infrastructure.constant.VoucherStatus;
 import com.ndt.be_stepupsneaker.util.ConvertTime;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class AutoScheduledServiceImpl implements AutoScheduledService{
     private AdminVoucherRepository adminVoucherRepository;
     private AdminOrderRepository adminOrderRepository;
+    private AdminOrderHistoryRepository adminOrderHistoryRepository;
 
 
     @Autowired
-    public AutoScheduledServiceImpl(AdminVoucherRepository adminVoucherRepository, AdminOrderRepository adminOrderRepository) {
+    public AutoScheduledServiceImpl(AdminVoucherRepository adminVoucherRepository, AdminOrderRepository adminOrderRepository, AdminOrderHistoryRepository adminOrderHistoryRepository) {
         this.adminVoucherRepository = adminVoucherRepository;
         this.adminOrderRepository = adminOrderRepository;
+        this.adminOrderHistoryRepository = adminOrderHistoryRepository;
     }
 
     @Override
@@ -46,6 +52,7 @@ public class AutoScheduledServiceImpl implements AutoScheduledService{
     }
 
     @Override
+    @Transactional
     public void updateOrderAutomatically() {
         long currentMillis = Instant.now().toEpochMilli();
         // Calculate the time 30 minutes ago in milliseconds
@@ -55,6 +62,11 @@ public class AutoScheduledServiceImpl implements AutoScheduledService{
 
         if (!expiredOrders.isEmpty()){
             System.out.println("========================== AUTO UPDATE ORDER =======================");
+            List<UUID> expiredOrderIds = expiredOrders.stream()
+                    .map(Order::getId)
+                    .collect(Collectors.toList());
+
+            adminOrderHistoryRepository.deleteAllByOrder(expiredOrderIds);
             adminOrderRepository.deleteAllInBatch(expiredOrders);
         }
     }
