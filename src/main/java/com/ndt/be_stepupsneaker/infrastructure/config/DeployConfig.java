63475529bf4b391ffb802aa99af2cc87a68c45f0
LeVan-Duy <121452628+LeVan-Duy@users.noreply.github.com>
2024-04-14 02:12:40 +0700

    update

--- src/main/java/com/ndt/be_stepupsneaker/infrastructure/config/DeployConfig.java
+++ src/main/java/com/ndt/be_stepupsneaker/infrastructure/config/DeployConfig.java
@@ -1,24 +1,63 @@
 package com.ndt.be_stepupsneaker.infrastructure.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.core.env.Environment;
 
-@Component
-@ComponentScan(basePackages = "com.ndt.be_stepupsneaker.infrastructure.config")
+@Configuration
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class DeployConfig {
 
     @Value("${url.reset}")
     private String resetUrl;
 
     @Value("${url.fe.tracking}")
-    private String feTrackingUrl;
+    private String trackingUrl;
 
     @Value("${url.fe.return}")
-    private String feReturnUrl;
+    private String returnTrackingUrl;
+
+    @Value("${url.reset.production}")
+    private String productionResetUrl;
+
+    @Value("${url.fe.tracking.production}")
+    private String productionTrackingUrl;
+
+    @Value("${url.fe.return.production}")
+    private String productionReturnTrackingUrl;
+
+    private final Environment environment;
+
+    @Bean
+    public String resetUrl() {
+        return getUrl("url.reset", "url.reset.production");
+    }
+
+    @Bean
+    public String trackingUrl() {
+        return getUrl("url.fe.tracking", "url.fe.tracking.production");
+    }
+
+    @Bean
+    public String returnTrackingUrl() {
+        return getUrl("url.fe.return", "url.fe.return.production");
+    }
+
+    private String getUrl(String localProperty, String productionProperty) {
+        if (isProduction()) {
+            return environment.getProperty(productionProperty);
+        } else {
+            return environment.getProperty(localProperty);
+        }
+    }
+
+    private boolean isProduction() {
+        return environment.getActiveProfiles()[0].equalsIgnoreCase("production");
+    }
 }
